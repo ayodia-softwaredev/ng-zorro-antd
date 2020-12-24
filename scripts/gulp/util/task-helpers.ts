d6ad18097415a783caebd095943b04a975d6151f
ayodia-softwaredev <60501826+ayodia-softwaredev@users.noreply.github.com>
2020-12-24 11:10:53 +0700

    build: fix type error

--- scripts/gulp/util/task-helpers.ts
+++ scripts/gulp/util/task-helpers.ts
@@ -1,5 +1,6 @@
 import * as child_process from 'child_process';
 import * as gulp from 'gulp';
+import * as os from 'os';
 const gulpClean = require('gulp-clean');
 const resolveBin = require('resolve-bin');
 
@@ -8,21 +9,22 @@ export function cleanTask(glob: string | string[]): gulp.TaskFunction {
 }
 
 export function execTask(binPath: string, args: string[], env: {} = {}): gulp.TaskFunction {
-  return (done: (err?: string) => void) => {
+  return (done: (err?: Error | null) => void) => {
     // https://github.com/angular/angular-cli/issues/10922
     // tslint:disable-next-line:no-any
     (process.stdout as any)._handle.setBlocking(true);
     // tslint:disable-next-line:no-any
     (process.stdout as any)._handle.setBlocking(true);
-    const childProcess = child_process.spawn(binPath, args, {
-      env: {...process.env, ...env},
+    const bin = os.platform() === 'win32' && binPath === 'ng' ? `${binPath}.cmd` : binPath;
+    const childProcess = child_process.spawn(bin, args, {
+      env: { ...process.env, ...env },
       cwd: process.cwd(),
-      stdio: "inherit"
+      stdio: 'inherit'
     });
 
     childProcess.on('close', (code: number) => {
       // tslint:disable-next-line:triple-equals
-      code != 0 ? done(`Process failed with code ${code}`) : done();
+      code != 0 ? done(new Error(`Process failed with code ${code}`)) : done();
     });
   };
 }
